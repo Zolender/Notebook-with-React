4f99a3ece6f4949c771c4ec3b1c3f18ff4432d26
Zolender <ndeingare@gmail.com>
2026-02-24 16:17:10 +0200

    feat: add NoteList component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import SideBar from './Components/SideBar'
+import NoteList from './Components/NoteList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='grid grid-cols-3 w-full min-h-screen bg-slate-800'>
+    <div className='flex  w-full min-h-screen bg-slate-800'>
         <SideBar />
+        <NoteList />
     </div>
   )
 }
---
+++ src/Components/NoteList.jsx
@@ -0,0 +1,12 @@
+const NoteList = () => {
+    return (
+        <div className="flex flex-col items-start w-70 px-2 py-5 gap-5 border-4">
+            <div className="relative bg-slate-600 w-full border-4 rou">
+                <input type="text" value={'Search'} className="border-none px-3 py-1 "/>
+            </div>
+            
+        </div>
+    );
+}
+
+export default NoteList;
