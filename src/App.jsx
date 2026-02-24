8f4706a8e3642b7369723be1cb4a51d9f5268682
Zolender <ndeingare@gmail.com>
2026-02-24 15:30:56 +0200

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Created main application structure including App component and SideBar component.
    - Added SVG assets for Vite and React logos.
    - Configured Vite with React and Tailwind CSS plugins.
    - Included global CSS file for Tailwind CSS styles.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='grid grid-cols-3 w-full'>
+        SideBare
+    </div>
+  )
+}
+
+export default App
