ee4e61a2836da072b1fcf179c279103c9213f1e3
Zolender <ndeingare@gmail.com>
2026-02-24 16:24:26 +0200

    feat: update NoteList component layout and styling

--- src/Components/NoteList.jsx
+++ src/Components/NoteList.jsx
@@ -1,9 +1,17 @@
+import { Search, Wind } from "lucide-react";
+
 const NoteList = () => {
     return (
-        <div className="flex flex-col items-start w-70 px-2 py-5 gap-5 border-4">
-            <div className="relative bg-slate-600 w-full border-4 rou">
-                <input type="text" value={'Search'} className="border-none px-3 py-1 "/>
+        <div className="flex flex-col items-start w-72 px-3 py-5 gap-5 border-r-2 border-slate-400 text-slate-100">
+            <p className="text-xl font-bold">All Notes</p>
+            <div className="relative bg-slate-600 w-full rounded-sm flex items-center px-2">
+                <Search size={22} className="" />
+                <input type="text" value={'Search notes...'} className="border-none px-3 py-1 outline-none"/>
             </div>
+            <div className="flex gap-2 justify-center items-center w-full">
+                <Wind size={22} />
+                No notes yet...
+                </div>
             
         </div>
     );
