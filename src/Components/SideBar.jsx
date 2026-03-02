8290ab33dded7828fb7341f3769b40a10104299b
Zolender <ndeingare@gmail.com>
2026-03-02 19:05:43 +0200

    feat: improve SideBar component with enhanced transition effects and layout adjustments

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,13 +4,13 @@ const SideBar = ({onAdd, setIsSideBarExpanded, isSideBarExpanded}) => {
 
 
     return (
-        <div className={`transition-all duration-50 border-r-2 border-slate-700 h-screen flex flex-col bg-slate-800 overflow-hidden ${isSideBarExpanded ? 'w-64' : 'w-16'}`}>            
-            <div className={`py-2 flex ${isSideBarExpanded? 'justify-start ml-4': 'justify-center'}`}>
+        <div className={`transition-all duration-300 border-r-2 border-slate-700 h-screen flex flex-col bg-slate-800 overflow-hidden ${isSideBarExpanded ? 'w-64' : 'w-16'}`}>            
+            <div className={`py-2 flex justify-start pl-4`}>
                 <button type="button">
                     <Menu size={35} onClick={()=>setIsSideBarExpanded(!isSideBarExpanded)} className="p-2 hover:bg-slate-700 rounded-lg self-end mb-4 text-slate-300 "/>
                 </button>
             </div>
-            <button onClick={onAdd} type="button" className={`flex justify-center items-center mx-auto  text-center py-1 bg-blue-600 hover:bg-blue-700 active:bg-blue-900 hover:cursor-pointer rounded-lg border-none focus:outline-none transition-colors mb-2 text-slate-100 ${isSideBarExpanded? 'px-10 ': 'px-2'}`} >
+            <button onClick={onAdd} type="button" className={`flex justify-center items-center mx-auto  text-center py-1 bg-blue-600 hover:bg-blue-700 active:bg-blue-900 hover:cursor-pointer rounded-lg border-none focus:outline-none transition-colors mb-2 text-slate-100 ${isSideBarExpanded? 'w-[90%]': 'px-2'}`} >
                 <PencilLine size={22} />
                 {isSideBarExpanded && <span className="ml-3">New Note</span>}
             </button>
@@ -29,7 +29,7 @@ const SideBar = ({onAdd, setIsSideBarExpanded, isSideBarExpanded}) => {
                     {isSideBarExpanded && <span className="ml-3">Recent</span>}
                 </p>
                 <p className={` hover:text-slate-100 hover:cursor-pointer rounded-sm py-2 flex items-center gap-2 transition ${isSideBarExpanded? "pl-2 w-[70%] hover:bg-slate-500": ''}`}>
-                    <Trash2 />
+                    <Trash2 size={22} />
                     {isSideBarExpanded && <span className="ml-3">Trash</span>}
                 </p>
             </div>
